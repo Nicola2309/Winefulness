2feefc5badd20deb1ac1a41a715aef595c2ccdc5
Nicola2309 <nicola.spigaglia@outlook.it>
2021-04-15 16:44:36 +0000

    Functionality: quantity boxes in shopping bag page

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,6 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('<product_id>', views.wine_details, name='wine_details'),
 ]
--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_wines, name='products'),
+    path('<product_id>', views.wine_details, name="wine_details"),
 ]
