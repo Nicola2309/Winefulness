f038e81686855000940b37d642d6f2e6eaf58d51
Nicola2309 <nicola.spigaglia@outlook.it>
2021-04-28 09:49:18 +0000

    Function: added product view, url and template for the Product Admin

--- products/views.py
+++ products/views.py
@@ -5,6 +5,7 @@ from django.db.models import Q
 from django.db.models.functions import Lower
 
 from .models import Product, Category
+from .forms import ProductForm
 
 
 # Create your views here.
@@ -69,3 +70,14 @@ def wine_details(request, product_id):
     }
 
     return render(request, 'products/wine_details.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
