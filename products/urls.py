9eb7735532800e214a38b164b81fcfecff031eb2
Nicola2309 <nicola.spigaglia@outlook.it>
2021-04-28 11:00:24 +0000

    Function: editing products for Products Admin

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.all_wines, name='products'),
     path('<int:product_id>/', views.wine_details, name="wine_details"),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
