b4fc587b229eac0ef44275f2e47985abf7a0231f
Nicola2309 <nicola.spigaglia@outlook.it>
2021-05-21 09:57:16 +0000

    Winemaker Model created

--- winemakers/models.py
+++ winemakers/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Winemakers(models.Model):
+    class Meta:
+        verbose_name_plural = 'Winemakers'
+
+    title = models.CharField(max_length=120)
+    heading = models.CharField(max_length=150, null=False, blank=False)
+    description = models.TextField(max_length=254, null=False, blank=False)
+    content = models.TextField(null=False, blank=False)
+    image_url = models.URLField(
+        max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
