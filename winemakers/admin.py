2f77697bf48a460cd364ce1948d8b842747effb2
Nicola2309 <nicola.spigaglia@outlook.it>
2021-05-21 10:45:48 +0000

    CommentAdmin and edit_comment.html page created

--- winemakers/admin.py
+++ winemakers/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Winemakers, Comments
 
-# Register your models here.
+
+class WinemakersAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'heading',
+        'description',
+        'content',
+        'image',
+    )
+
+
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = (
+        'winemakers',
+        'user',
+        'date_time',
+        'comment',
+    )
+
+
+admin.site.register(Winemakers, WinemakersAdmin)
+admin.site.register(Comments, CommentsAdmin)
