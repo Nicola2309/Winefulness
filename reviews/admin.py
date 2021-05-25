acb8ef2eebe36ca041bdd9bd690659140c604b16
Nicola2309 <nicola.spigaglia@outlook.it>
2021-05-25 13:13:58 +0000

    Created reviewAdmin and forms.py in reviews

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    """
+    Admin display for reviews
+    """
+    list_display = (
+        'product',
+        'review_title',
+        'created_on',
+        'user_profile',
+    )
+    list_filter = ("product",)
+    search_fields = ['product', 'review_title', 'review_content']
+
+
+admin.site.register(Review, ReviewAdmin)
