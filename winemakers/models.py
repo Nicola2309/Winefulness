46c4eb6f608e958a62800f2ea906cacfa1d6b2f2
Nicola2309 <nicola.spigaglia@outlook.it>
2021-05-27 21:41:33 +0000

    Changed null=True in default='' where highlighted by the IDE

--- winemakers/models.py
+++ winemakers/models.py
@@ -8,11 +8,11 @@ class Winemakers(models.Model):
 
     producer_name = models.CharField(max_length=120)
     heading = models.CharField(max_length=150, null=False, blank=False)
-    location = models.TextField(max_length=254, null=False, blank=False)
+    location = models.CharField(max_length=120, null=False, blank=False)
     content = models.TextField(null=False, blank=False)
     image_url = models.URLField(
-        max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+        max_length=1024, default='', blank=True)
+    image = models.ImageField(default='', blank=True)
 
     def __str__(self):
         return self.producer_name
